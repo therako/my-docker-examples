38ebbe62671ca877147a2c5fc896fdf1b237c6f3
AK <ak@thecarousell.com>
2018-02-25 18:25:59 +0800

    Removing AVX2 support.

--- tensorflow/generate_packages_script.sh
+++ tensorflow/generate_packages_script.sh
@@ -4,7 +4,7 @@
 printf "/usr/bin/python3\n\ny\ny\nn\nn\nn\nn\nn\nn\nn\n" | ./configure
 
 # Build tools
-bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 -k //tensorflow/tools/pip_package:build_pip_package
+bazel build -c opt --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-mfma --copt=-mfpmath=both -k //tensorflow/tools/pip_package:build_pip_package
 
 # Generate python whl package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /code/tensorflow_pkg
