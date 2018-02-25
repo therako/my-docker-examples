c7a6883948e0b70b3ad08095d268518f3740ca18
AK <ak@thecarousell.com>
2018-02-25 14:29:46 +0800

    First example, building tensorflow custom package.

---
+++ tensorflow/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+docker build . -t tensorflow-builder
+docker stop tensorflow-builder | true
+docker rm tensorflow-builder | true
+docker run -d -v $(pwd)/tensorflow_pkg:/code/tensorflow_pkg -d --privileged --name tensorflow-builder tensorflow-builder ./generate_packages_script.sh
+
+docker logs -f tensorflow-builder
